c43517ca01242f2137349ed0bb4006b81155235a
Min <esspacetoM@gmail.com>
2023-06-10 19:55:03 +0900

    Animation example

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 function App() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Box
+        transition={{ type: "spring", bounce: 0.5, delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
+    </Wrapper>
+  );
 }
 
 export default App;
